4149a5fb9a8c11cdeac57ccf5810f06b5e343b0e
Anton Strandqvist <anton.sjolander@hyperisland.se>
2024-05-20 14:30:16 +0200

    added animation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,37 @@
+import { gsap } from "gsap";
 const img = document.querySelector("img");
 const imgSize = img.getBoundingClientRect();
 const c = document.getElementById("canvas");
 c.width = imgSize.width;
 c.height = imgSize.height;
 const ctx = c.getContext("2d");
-ctx.drawImage(img, 0, 0);
+
+// Turn of image smoothing to get pixalted effect
+ctx.imageSmoothingEnabled = false;
+ctx.webkitImageSmoothingEnabled = false;
+ctx.mozImageSmoothingEnabled = false;
+
+const blocks = {
+	amount: 1,
+};
+
+function pixalte() {
+	requestAnimationFrame(pixalte);
+	const size = blocks.amount * 0.01;
+
+	const w = c.width * size;
+	const h = c.height * size;
+	ctx.drawImage(img, 0, 0, w, h);
+
+	ctx.drawImage(c, 0, 0, w, h, 0, 0, c.width, c.height);
+}
+pixalte();
+
+gsap.to(blocks, {
+	amount: 100,
+	delay: 2,
+	ease: "steps(100)",
+	duration: 6,
+	repeat: -1,
+	yoyo: true,
+});
